4d5463b2b333169a1a470b6bd930ebb97bb7aec8
Bob F <bobfdev@gmail.com>
2022-02-21 20:08:30 -0600

    initial files

---
+++ app/root.jsx
@@ -0,0 +1,25 @@
+import { Outlet, LiveReload } from 'remix'
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  )
+}
+
+function Document({ children, title }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>{ title ? title : "Remix Blog" }</title>
+      </head>
+      <body>
+        {children}
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
+      </body>
+    </html>
+  )
+}
+
+function Layout({})
